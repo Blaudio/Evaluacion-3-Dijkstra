160c6bacf13403a99840a84ac3e16f861a31f60a
Blaudio <bvargas_mont@hotmail.com>
2015-09-15 18:24:18 -0300

    Update Grafos.c

--- Grafos.c
+++ Grafos.c
@@ -1,1 +1,10 @@
+#include <stdio.h>
+#define NODOS 7
 
+typedef struct Nodo{
+  char nombre[NODOS];
+  char color[NODOS];
+  int distancia[NODOS];
+  int coste[NODOS][NODOS];
+  int padre[NODOS];
+}
