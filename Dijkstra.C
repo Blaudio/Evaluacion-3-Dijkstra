cb3f175a7bf80f3fa0aeb6df48ee6fdd96987cfc
Blaudio <bvargas_mont@hotmail.com>
2015-09-19 13:38:06 -0300

    Update Dijkstra.C

--- Dijkstra.C
+++ Dijkstra.C
@@ -4,3 +4,10 @@ typedef struct Dijkstra{
   int padre[NODOS]
   int peso[NODOS];
 }
+
+Dijkstra *inicializa (Dijkstra lista){
+  int contador;
+  for(contador=0; contador<=Nodos; contador++){
+    lista->padre[contador]=NULL;
+    lista->peso[contador]=999;
+}
