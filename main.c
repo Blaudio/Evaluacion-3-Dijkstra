6241748767dfb7ca5067a69ff3eca85e9f36a051
Blaudio <bvargas_mont@hotmail.com>
2015-09-26 17:00:03 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Grafos.h"
+#include "Dijkstra.h"
 
+int main(){
+  Nodo *grafo =inicializa_nodo();
+  *grafo =agrega_elementos_nodo(grafo);
+  Dijkstra *lista =*inicializa_lista();
+  *busqueda(grafo, lista, 4);//se comiensa la busqueda con la letra E
+}
