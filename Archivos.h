6241139bfc84b834f8f835db2262cd0163e92294
Blaudio <bvargas_mont@hotmail.com>
2015-09-26 17:25:56 -0300

    Update Archivos.h

--- Archivos.h
+++ Archivos.h
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define NODOS 7
 
+void generador_mapa(Dijkstra lista){
+  FILE* fichero;
+  char columna[80] = "c:\\grafo.txt";
+  char fila[81];
+  fichero = fopen ("c:\\grafo.txt", "wt");
+  for(contador=0; contador<=Nodos; contador++);{
+  //  fputs("?????????????\n",lista->padre,lista->padre,lista->padre,fichero);
+  }
+  fclose(fichero);           
+}
