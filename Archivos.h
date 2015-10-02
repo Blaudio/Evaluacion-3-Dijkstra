c541b394b4c556e09d76c3c8be57359f8b76b243
Blaudio <bvargas_mont@hotmail.com>
2015-10-02 19:33:54 -0300

    Update Archivos.h

--- Archivos.h
+++ Archivos.h
@@ -4,11 +4,17 @@
 
 void generador_mapa(Dijkstra lista){
   FILE* fichero;
-  char columna[80] = "c:\\grafo.txt";
-  char fila[81];
-  fichero = fopen ("c:\\grafo.txt", "wt");
-  for(contador=0; contador<=Nodos; contador++);{
-  //  fputs("?????????????\n",lista->padre,lista->padre,lista->padre,fichero);
+  //char columna[80] = "c:\\grafo.dot";
+  //char fila[81];
+  fichero = fopen ("c:\\grafo.dot", "wt");
+  fprintf(fichero,"disgraph grafo{\n");
+  fprintf(fichero,"  splines = true;\n");
+  fprintf(fichero,"  node [style=filled, fillcolor=white, fontcolor=black];\n");
+  fprintf(fichero,"  edge [color=black];\n");
+  for(contador=0; contador<=NODOS; contador++);{
+    fprintf(fichero,"  %i [label=\"%s\"];\n",contador, lista->color[contador]);
   }
-  fclose(fichero);           
+  fprintf(fichero,"}\n");
+  fclose(fichero);      
+  return(1);
 }
