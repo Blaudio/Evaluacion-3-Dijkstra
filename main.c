7eef18345757d449f5eb7bc13c98e28b16b97cac
Blaudio <bvargas_mont@hotmail.com>
2015-10-19 17:41:45 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "Archivos.h"
+#include "Dijkstra.h"
+#define ELEMENTO_INICIAL 4
 
 int main(){
   Nodo *grafo =inicializa_nodo();
   grafo =agrega_elementos_nodo(grafo);
-  Dijkstra *lista =inicializa_lista();
-  lista=busqueda(grafo, lista, 4);//se comiensa la busqueda con la letra E
-  generador_mapa(lista, grafo);
+  Dijkstra *lista;
+  lista=inicializa_lista();
+  int i;
+  generador_mapa();
+  lista->peso[ELEMENTO_INICIAL]=0;
+  lista=busqueda(grafo, lista, ELEMENTO_INICIAL);//se comiensa la busqueda con la letra E
+  finaliza_generador_mapa();
 }
